bc33d13574bfa9f4fb39d0a6d7e31ce479ccd8ba
rohitsingh2306 <99877302+rohitsingh2306@users.noreply.github.com>
2022-09-06 22:19:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Dream Project</h1>
+      <p data-ns-test="project-description">Project Description</p>
+    </div>
+  );
+};
 
 export default App;
